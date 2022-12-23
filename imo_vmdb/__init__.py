f705123b07805fe9e6723fa55d312f73c1ddbcbc
Janko Richter <j.richter@businessangels.de>
2022-12-23 11:48:32 +0100

    Python packaging with Poetry

--- imo_vmdb/__init__.py
+++ imo_vmdb/__init__.py
@@ -3,8 +3,6 @@ import imo_vmdb.command.import_csv
 import imo_vmdb.command.initdb
 import imo_vmdb.command.normalize
 
-__version__ = '1.3.1'
-
 cleanup = imo_vmdb.command.cleanup.main
 import_csv = imo_vmdb.command.import_csv.main
 initdb = imo_vmdb.command.initdb.main
